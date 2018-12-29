eae44abc36e631e9eb99ff74be73061aee9fbdca
Jake Bédard <32440974+Clearedkinkajou@users.noreply.github.com>
2018-12-29 16:22:04 -0500

    BLPHNX 20: Vehicle Name in Table View (#40)
    * BLPHNX11 - TableView Updates in Real Time
    
    -Added the ability to see character names
    -Added local time when events happen
    -Added what continent kills occured
    -Added if an event type is a kill or death
    
    * BLPHX11 - Small Fixes
    
    * BLPHX11 - Added new features to KillBoard Tab
    
    -Added faction colors for character names
    -API calls happen before updating the table view instead of during
    -Added 6 Labels to indicate statistics in the tableView
    -Moved static words into the resource bundle
    -The GUI and backend can now work independently
    
    * BLPHX11 - Added numbered rows
    
    * BLPHX20 - Added Vehicles names
    
    -Vehicle ID's now display the vehicle name in the Table View
    -Columns have max Widths so that they don't take too much space
    -Dates have been formated to yyyy-MM-dd HH:mm:ss
    -Small fixes to ensure that Id's aren't being called more than once in a Service

--- src/main/java/com/application/KillBoardController.java
+++ src/main/java/com/application/KillBoardController.java
@@ -1,12 +1,10 @@
 package com.application;
 
-import com.objects.Casualty;
-import com.objects.CasualtyDisplay;
-import com.objects.Item;
-import com.objects.Player;
+import com.objects.*;
 import com.services.CasualtyService;
 import com.services.ItemService;
 import com.services.PlayerService;
+import com.services.VehicleService;
 import com.utilities.Continents;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
@@ -17,6 +15,7 @@ import javafx.scene.paint.Color;
 import javafx.util.Callback;
 
 import java.net.URL;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class KillBoardController implements Initializable {
@@ -41,23 +40,20 @@ public class KillBoardController implements Initializable {
 
     private Map<String, Player> players;
     private Map<String, Item> items;
+    private Map<String, Vehicle> vehicles;
     private int headShots;
     private int numberOfKills;
     private String userName;
+    private final SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         bundle = resources;
-
         ((TableColumn) tableView.getColumns().get(4)).setCellValueFactory(new PropertyValueFactory<CasualtyDisplay, String>("attackerCharacterName"));
         ((TableColumn) tableView.getColumns().get(4)).setCellFactory(getValue());
 
         ((TableColumn) tableView.getColumns().get(5)).setCellValueFactory(new PropertyValueFactory<CasualtyDisplay, String>("targetCharacterName"));
         ((TableColumn) tableView.getColumns().get(5)).setCellFactory(getValue());
-
-        players = new HashMap<String, Player>();
-        items = new HashMap<String, Item>();
-
     }
 
     private Callback<TableColumn, TableCell> getValue() {
@@ -96,11 +92,16 @@ public class KillBoardController implements Initializable {
         LinkedList<CasualtyDisplay> casualtyDisplayList = new LinkedList<>();
         PlayerService playerService = new PlayerService();
         ItemService itemService = new ItemService();
+        VehicleService vehicleService = new VehicleService();
         progressBar.setVisible(true);
         Platform.runLater(() -> battleRank.setText(player.getPrestigeLevel() + "~" + player.getBattleRank().getBattleRankValue()));
         ArrayList<String> playerIDs = new ArrayList<>();
         playerIDs.add(player.getCharacterID());
         ArrayList<String> weaponIds = new ArrayList<>();
+        ArrayList<String> vehicleIds = new ArrayList<>();
+        Platform.runLater(() -> {
+            progressBar.setProgress(0.1);
+        });
         for (Casualty casualty : casualtyList) {
             if (!playerIDs.contains(casualty.getCharacterID())) {
                 playerIDs.add(casualty.getCharacterID());
@@ -108,16 +109,32 @@ public class KillBoardController implements Initializable {
             if (!playerIDs.contains(casualty.getAttackerCharacterID())) {
                 playerIDs.add(casualty.getAttackerCharacterID());
             }
-            if (!casualty.getAttackerWeaponID().equals("0")) {
+            if (!casualty.getAttackerWeaponID().equals("0") && !weaponIds.contains(casualty.getAttackerWeaponID())) {
                 weaponIds.add(casualty.getAttackerWeaponID());
             }
+            if(!casualty.getAttackerVehicleID().equals("0") && !vehicleIds.contains(casualty.getAttackerVehicleID())){
+                vehicleIds.add(casualty.getAttackerVehicleID());
+            }
         }
+        Platform.runLater(() -> {
+            progressBar.setProgress(0.2);
+        });
         players = playerService.getPlayersByIds(playerIDs);
+        Platform.runLater(() -> {
+            progressBar.setProgress(0.4);
+        });
         items = itemService.getItemsByIDs(weaponIds);
+        Platform.runLater(() -> {
+            progressBar.setProgress(0.6);
+        });
+        vehicles = vehicleService.getVehiclesByIds(vehicleIds);
+        Platform.runLater(() -> {
+            progressBar.setProgress(0.75);
+        });
         for (Casualty casualty : casualtyList) {
             CasualtyDisplay casualtyDisplay = new CasualtyDisplay();
-            casualtyDisplay.setRowNumber(String.valueOf(casualtyDisplayList.size()+1));
-            if(casualty.getTableType().equals("kills") && casualty.getAttackerCharacterID().equals(casualty.getCharacterID())){
+            casualtyDisplay.setRowNumber(String.valueOf(casualtyDisplayList.size() + 1));
+            if (casualty.getTableType().equals("kills") && casualty.getAttackerCharacterID().equals(casualty.getCharacterID())) {
                 continue;
             }
             casualtyDisplay.setAttackerCharacterName(casualty.getAttackerCharacterID());
@@ -127,7 +144,7 @@ public class KillBoardController implements Initializable {
             } else if (casualty.getAttackerWeaponID().equals("0") && casualty.getAttackerVehicleID().equals("0")) {
                 casualtyDisplay.setAttackerWeaponName("Unknown");
             } else if (casualty.getAttackerWeaponID().equals("0")) {
-                casualtyDisplay.setAttackerWeaponName(casualty.getAttackerVehicleID());
+                casualtyDisplay.setAttackerWeaponName(vehicles.get(casualty.getAttackerVehicleID()).getVehicleName().getEnglish());
             } else {
                 casualtyDisplay.setAttackerWeaponName(items.get(casualty.getAttackerWeaponID()).getItemName().getEnglish());
             }
@@ -142,19 +159,21 @@ public class KillBoardController implements Initializable {
             }
             casualtyDisplay.setTableType(bundle.getString(casualty.getTableType()));
             Date date = new Date(Long.parseLong(casualty.getTimestamp()) * 1000);
-            casualtyDisplay.setDateLocalTime(date.toString());
+
+            casualtyDisplay.setDateLocalTime(format.format(date));
 
             casualtyDisplayList.add(casualtyDisplay);
-            Platform.runLater(() -> {
-                progressBar.setProgress(((double) casualtyDisplayList.size()) / casualtyList.size());
-                tableView.getItems().add(casualtyDisplay);
-                kills.setText(String.valueOf(numberOfKills));
-                deaths.setText(String.valueOf(casualtyDisplayList.size() - numberOfKills));
-                total.setText(String.valueOf(casualtyDisplayList.size()));
-                headshotRate.setText(((double) Math.round((((double) headShots) / (double) numberOfKills) * 10000)) / 100 + "%");
-                trueKD.setText(String.valueOf(Math.round((double) numberOfKills / ((double) (casualtyDisplayList.size() - numberOfKills)) * 100) / 100));
-            });
         }
         progressBar.setVisible(false);
+        Platform.runLater(() -> {
+            tableView.getItems().addAll(casualtyDisplayList);
+            kills.setText(String.valueOf(numberOfKills));
+            deaths.setText(String.valueOf(casualtyDisplayList.size() - numberOfKills));
+            total.setText(String.valueOf(casualtyDisplayList.size()));
+            headshotRate.setText(((double) Math.round((((double) headShots) / (double) numberOfKills) * 10000)) / 100 + "%");
+            trueKD.setText(String.valueOf(Math.round((double) numberOfKills / ((double) (casualtyDisplayList.size() - numberOfKills)) * 100) / 100));
+            tableView.refresh();
+            progressBar.setProgress(1.0);
+        });
     }
 }
