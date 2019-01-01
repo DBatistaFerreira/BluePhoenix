7e3ab504323374843dae28b33304938639f6f6a2
Jake Bédard <32440974+Clearedkinkajou@users.noreply.github.com>
2019-01-01 14:55:13 -0500

    BLPHNX24 - Fix search button Issues (#51)
    -Made it possible to simply press enter while in the textfield to search
    -Made it so the textfield clears out text whenever the search button is pressed
    -Added a condition that makes it so if the textfield has no words than it won't search

--- src/main/java/com/application/PlayerLookupController.java
+++ src/main/java/com/application/PlayerLookupController.java
@@ -3,6 +3,7 @@ package com.application;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -15,7 +16,11 @@ public class PlayerLookupController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        playerName.setOnKeyPressed(key -> {
+            if(key.getCode().equals(KeyCode.ENTER)){
+                search();
+            }
+        });
     }
 
     public void setRootController(SceneController sceneController) {
@@ -23,7 +28,10 @@ public class PlayerLookupController implements Initializable {
     }
 
     @FXML
-    private void search(){
-        sceneController.createKillBoard(playerName.getText());
+    private void search() {
+        if (!playerName.getText().isEmpty()) {
+                sceneController.createKillBoard(playerName.getText());
+        }
+        playerName.setText(null);
     }
 }
