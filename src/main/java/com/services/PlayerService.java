9b1bca21a3f02f31f669ad78798a2cbaee1b8bf8
Daniel Batista-Ferreira <danielbatfer@hotmail.com>
2019-01-01 18:54:36 -0500

    BLPHNX25:    *Fixed 414 error    *Partition lists    *Added guava library    *Fixed case if list was null

--- src/main/java/com/services/PlayerService.java
+++ src/main/java/com/services/PlayerService.java
@@ -1,5 +1,6 @@
 package com.services;
 
+import com.google.common.collect.Lists;
 import com.google.gson.Gson;
 import com.objects.Player;
 import com.utilities.RESTConsumer;
@@ -17,6 +18,7 @@ public class PlayerService {
 
     private static final String SERVICE_KEY = "s:troopers";
     private static Logger logger = Logger.getLogger(PlayerService.class.getName());
+    private static final int PARTITION_SIZE = 350;
     private Player player;
     private HashMap<String, Player> players = new HashMap<>();
 
@@ -35,43 +37,51 @@ public class PlayerService {
     }
 
     public Map<String, Player> getPlayersByIds(List<String> listOfIDs){
-        String commaList = getStringListOfIDs(listOfIDs);
-        logger.info("Retrieving players for playerIDs: " + commaList);
-        try {
+        for(List<String> partition : Lists.partition(listOfIDs, PARTITION_SIZE)) {
+            String commaList = getStringListOfIDs(partition);
             String playerResponse = RESTConsumer.get("http://census.daybreakgames.com/" + SERVICE_KEY + "/get/ps2:v2/character/?character_id=" + commaList + "&c:resolve=outfit_member_extended");
+            insertPlayers(playerResponse);
+        }
+        logger.info("Successfully retrieved all players");
+        return players;
+    }
+
+    private void insertPlayers(String playerResponse){
+        try {
             JSONObject jsonObject = new JSONObject(playerResponse);
             JSONArray jsonArray = new JSONArray(jsonObject.getString("character_list"));
             Gson gson = new Gson();
             ArrayList<Player> playerList = new ArrayList<>();
-            for(int i = 0; i < jsonArray.length(); i++) {
+            for (int i = 0; i < jsonArray.length(); i++) {
                 playerList.add(gson.fromJson(jsonArray.get(i).toString(), Player.class));
                 Player currentPlayer = playerList.get(i);
                 players.put(currentPlayer.getCharacterID(), currentPlayer);
             }
-            logger.info("Successfully retrieved all players");
-            return players;
         }
-        catch(JSONException e){
+        catch(JSONException e) {
             logger.info("Error in retrieving players");
             logger.info(e.getMessage());
         }
-        return null;
     }
 
     public static String getStringListOfIDs(List<String> listOfIDs){
         StringBuilder IDs = new StringBuilder();
-        IDs.append(listOfIDs.get(0));
-        IDs.append(",");
-        for(int i = 1; i < listOfIDs.size(); i++){
-            if(i != listOfIDs.size() - 1){
-                IDs.append(listOfIDs.get(i));
-                IDs.append(",");
+        if(listOfIDs.size() > 0) {
+            IDs.append(listOfIDs.get(0));
+            IDs.append(",");
+            for (int i = 1; i < listOfIDs.size(); i++) {
+                if (i != listOfIDs.size() - 1) {
+                    IDs.append(listOfIDs.get(i));
+                    IDs.append(",");
+                } else {
+                    IDs.append(listOfIDs.get(i));
+                }
             }
-            else{
-                IDs.append(listOfIDs.get(i));
-            }
+            return IDs.toString();
         }
-        return IDs.toString();
+        else{
+            return null;
+        }
     }
 
     private void convertJSONResponseToPlayer(String playerResponse){
